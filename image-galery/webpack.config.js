09e7e86f9629e1849ed5c790788137975f9a328e
Denis_Koval <den_hope@mail.ru>
2022-02-09 22:31:05 +0300

    feat: add copy plugin in webpacconfig

--- image-galery/webpack.config.js
+++ image-galery/webpack.config.js
@@ -3,11 +3,12 @@ const path = require("path");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-// const BundleAnalyzerPlugin = require("webpack-bundle-analyzer");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer");
 const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = (env, options) => {
   const isProduction = options.mode === "production";
+  const isAnalyze = env.analyze;
   const config = {
     mode: isProduction ? "production" : "development",
     devtool: isProduction ? "source-map" : "eval",
@@ -17,6 +18,9 @@ module.exports = (env, options) => {
       path: path.join(__dirname, "/dist"),
       filename: "script.js",
       assetModuleFilename: "assets/public/[hash][ext]",
+    },
+    optimization: {
+      minimize: false,
     },
     resolve: {
       extensions: [".js", ".json", ".mjs"],
@@ -61,12 +65,12 @@ module.exports = (env, options) => {
       new MiniCssExtractPlugin({
         filename: "style.css",
       }),
-      // new CopyPlugin({
-      //   patterns: [
-      //     { from: "./src/assets/img", to: "./assets/img" },
-      //     // { from: "./src/assets/image/", to: "./assets/image" },
-      //   ],
-      // }),
+      new CopyPlugin({
+        patterns: [
+          { from: "./src/assets/img", to: "./assets/img" },
+          { from: "./src/js/", to: "./assets/js" },
+        ],
+      }),
     ],
     // ...devServer()
   };
